7f568881cb4024a28b7fdb35ef04a6e87a49416d
Ihsan <122683406+rwixy@users.noreply.github.com>
2026-09-10 11:28:31 +0530

    feat: add event handler for resource stop to manage NUI focus

--- [core]/esx_whitelist/client/main.lua
+++ [core]/esx_whitelist/client/main.lua
@@ -1,3 +1,11 @@
 local WhitelistClient <const> = xLib.require "@esx_whitelist.client.module.whitelist.main"
 
 WhitelistClient.Init()
+
+AddEventHandler("onResourceStop", function(resourceName)
+    if GetCurrentResourceName() ~= resourceName then
+        return
+    end
+
+    SetNuiFocus(false, false)
+end)
